5f3fd94a85c7ea33d1c2f0495841322c97c8fa03
win_Oz <alericcusher25@gmail.com>
2023-07-10 01:24:43 +0530

    notes models

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Color(models.Model):
+    id = models.CharField(max_length=20)
+    color = models.CharField(max_length=7)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    id = models.CharField(max_length=20)
+    name = models.CharField(max_length=25)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class Note(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    title = models.CharField(max_length=100, null=True)
+    content = models.TextField(null=True)
+    tags = models.ManyToManyField(Tag)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
